4a69ed1f767bc4088a9f31ce9fe4cd7e19e4ef49
aishasiddiqah <aishasalam205@gmail.com>
2024-04-10 17:36:34 +0400

    Create index.js

---
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler (Bonus)/index.js
@@ -0,0 +1,17 @@
+var sound_1 = new Audio('Audio/ah-ha.mp3');
+
+var sound_2 = new Audio('Audio/dan.mp3');
+
+var sound_3 = new Audio('Audio/back-of-the-net.mp3');
+
+var sound_4 = new Audio('Audio/bangoutoforder.mp3');
+
+var sound_5 = new Audio('Audio/hellopartridge.mp3');
+
+var sound_6 = new Audio('Audio/emailoftheevening.mp3');
+
+var sound_7 = new Audio('Audio/imconfused.mp3');
+
+var sound_8 = new Audio('Audio/iateascotchegg.mp3');
+
+var sound_9 = new Audio('image+9th_audio_sample/a-trapped-boy-101soundboards.mp3');
