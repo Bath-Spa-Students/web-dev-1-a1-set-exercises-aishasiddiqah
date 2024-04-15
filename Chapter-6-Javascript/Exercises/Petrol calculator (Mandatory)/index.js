0b89e69420e962f14bb3821f571da12595a5cb9d
aishasiddiqah <144320364+aishasiddiqah@users.noreply.github.com>
2024-04-16 01:27:53 +0400

    Update index.js

--- Chapter-6-Javascript/Exercises/Petrol calculator (Mandatory)/index.js
+++ Chapter-6-Javascript/Exercises/Petrol calculator (Mandatory)/index.js
@@ -2,6 +2,7 @@ window.onload=()=>{
     const inputs = document.querySelectorAll("input");
 }
 
+//creating a function that will calculate the total price by multiplying two input values
 function calculateTotal(){
     const petrol=document.querySelector('#petrol').value;
     const liter=document.querySelector('#liter').value;
