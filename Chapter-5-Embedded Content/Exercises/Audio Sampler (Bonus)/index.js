29df37336e70f62af8e4bcaf3cee0463b6536b26
aishasiddiqah <144320364+aishasiddiqah@users.noreply.github.com>
2024-04-16 01:19:12 +0400

    Update index.js

--- Chapter-5-Embedded Content/Exercises/Audio Sampler (Bonus)/index.js
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler (Bonus)/index.js
@@ -1,17 +1,28 @@
+//creating a variable called sound_1 which contains a particular audio
 var sound_1 = new Audio('Audio/ah-ha.mp3');
 
+//creating a variable called sound_2 which contains a particular audio
 var sound_2 = new Audio('Audio/dan.mp3');
 
+//creating a variable called sound_3 which contains a particular audio
 var sound_3 = new Audio('Audio/back-of-the-net.mp3');
 
+//creating a variable called sound_4 which contains a particular audio
 var sound_4 = new Audio('Audio/bangoutoforder.mp3');
 
+//creating a variable called sound_5 which contains a particular audio
 var sound_5 = new Audio('Audio/hellopartridge.mp3');
 
+//creating a variable called sound_6 which contains a particular audio
 var sound_6 = new Audio('Audio/emailoftheevening.mp3');
 
+//creating a variable called sound_7 which contains a particular audio
 var sound_7 = new Audio('Audio/imconfused.mp3');
 
+//creating a variable called sound_8 which contains a particular audio
 var sound_8 = new Audio('Audio/iateascotchegg.mp3');
 
+//creating a variable called sound_9 which contains a particular audio
 var sound_9 = new Audio('image+9th_audio_sample/a-trapped-boy-101soundboards.mp3');
+
+//the audios will play when the variables are called
